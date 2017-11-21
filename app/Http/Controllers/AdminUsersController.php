f684a0997b67d2bfa5e05ff55588dcb3628a7965
hkrzysztof <hkrzysztof92@gmail.com>
2017-11-21 15:18:41 +0100

    Finished Post index + post creator. B4 categories

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -10,6 +10,7 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -47,12 +48,11 @@ class AdminUsersController extends Controller
     {
 
         if($request->password)
-
         $input = $request->all();
 
         if($file= $request->file('photo_id')){
             $name = time().'_'.$file->getClientOriginalName();
-            $file->move('images', $name);
+            $file->move('images/users', $name);
             $photo = Photo::create(['file'=>$name]);
             $input['photo_id'] = $photo->id;
         }
@@ -135,6 +135,14 @@ class AdminUsersController extends Controller
      */
     public function destroy($id)
     {
-        return view('admin.users.destroy');
+        $user = User::findOrFail($id);
+
+        unlink(public_path().$user->photo->file);
+
+        $user->delete();
+
+        Session::flash('deleted_user', 'The user has been deleted');
+
+        return redirect('/admin/users');
     }
 }
